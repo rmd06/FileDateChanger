48efacae131f013a7b60ca369eb8d7be640fb073
marlesu <tkk21@case.edu>
2017-04-28 23:04:08 -0400

    init

---
+++ ChangeFileDateLib/FileDateChanger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ChangeFileDateLib
+{
+    public class FileDateChanger
+    {
+        private FileInfo fileInfo;
+
+        public FileDateChanger(string filePath)
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+        }
+        public void ChangeCreationTime(DateTime timeToSet)
+        {
+            fileInfo.CreationTime = timeToSet;
+        }
+
+        public void ChangeLastAccessTime(DateTime timeToSet)
+        {
+            fileInfo.LastAccessTime = timeToSet;
+        }
+
+        public void ChangeLastWriteTime(DateTime timeToSet)
+        {
+            fileInfo.LastWriteTime = timeToSet;
+        }
+    }
+}
